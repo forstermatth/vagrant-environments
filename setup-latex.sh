133adc60c77de48a7871c7ad2b7f7b338f699edb
Matt Forster <forster.matth@gmail.com>
2014-04-07 17:14:33 -0600

    Update setup-latex.sh

--- setup-latex.sh
+++ setup-latex.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get -y git-core
+sudo apt-get -y install git
 git clone https://github.com/scottkosty/install-tl-ubuntu
 sudo ./install-tl-ubuntu --more-tex
+
+git config --global user.name "forstermatth"
+git config --global user.email forster.matth@gmail.com
